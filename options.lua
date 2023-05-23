753cd9523349d6c684ee1e35f220c040489b609e
Errorman76 <lwooyoung57@gmail.com>
2023-05-23 22:47:58 +0900

    cmdheight update by temporary fix

--- options.lua
+++ options.lua
@@ -9,6 +9,7 @@ return {
     wrap = false, -- sets vim.opt.wrap
     tabstop = 4, -- Number of space in a tab
     shiftwidth = 4, -- Number of space inserted for indentation
+    cmdheight = 1, -- temporary fix
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
