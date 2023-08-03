0f7ffb818a596172cfbcfa8576d86c2f97212caa
이우영 <aaron@heaventree.co.kr>
2023-08-03 14:52:19 +0900

    fix: community reset

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,29 +1,3 @@
 return {
   "AstroNvim/astrocommunity",
-
-  -- bash
-  {import = "astrocommunity.pack.bash"},
-
-  -- markdown 
-  {import = "astrocommunity.pack.markdown"},
-  {import = "astrocommunity.markdown-and-latex.markdown-preview-nvim"},
-
-  -- type & javascript
-  {import = "astrocommunity.pack.typescript"},
-  {import = "astrocommunity.pack.json"},
-  {import = "astrocommunity.pack.html-css"},
-  {import = "astrocommunity.pack.tailwindcss"},
-
-  -- python
-  {import = "astrocommunity.pack.python"},
-
-  -- go
-  {import = "astrocommunity.pack.go"},
-
-  -- docker
-  {import = "astrocommunity.pack.docker"},
-  
-  -- etc
-  {import = "astrocommunity.pack.toml"},
-  {import = "astrocommunity.pack.yaml"},
 }
