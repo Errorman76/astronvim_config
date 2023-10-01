03216d7991965fe9d595e4385b141190ff2aa49f
Errorman76 <lwooyoung57@gmail.com>
2023-10-01 17:16:18 +0900

    feat: add astrocommunity item

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,3 +1,20 @@
 return {
   "AstroNvim/astrocommunity",
+
+  -- lua
+  {import = "astrocommunity.pack.lua"},
+
+  -- bash
+  {import = "astrocommunity.pack.bash"},
+
+  -- markdown
+  {import = "astrocommunity.pack.markdown"},
+  {import = "astrocommunity.markdown-and-latex.markdown-preview-nvim"},
+
+  -- python
+  {import = "astrocommunity.pack.python"},
+
+  -- etc
+  {import = "astrocommunity.pack.toml"},
+  {import = "astrocommunity.pack.yaml"},
 }
